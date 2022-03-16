67be6a67d798ec3cca36efd996dca5cd2f5b4671
Jordan Litsas <jordanlitsas@live.com.au>
2022-03-16 13:52:02 +1100

    Calculator implemented

--- index.js
+++ index.js
@@ -1,18 +1,78 @@
-const express = require('express');
-const app = express();
+var express = require("express")
+var bodyParser=require('body-parser')
+var log = require('log');
+require("log-node")();
 
-app.get('/', (req, res) => {
-    console.log(`${getTimeAndServerActivityStamp()} Page Request`);
-    res.send("Welcome")
+app = express();
+// we set the port programmatically, in case we need to change it later
+var port = 3000;
+//this is where we are going to getch our html from
+var root = '/public'
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+
+//tell express to use the static middleware,
+app.use(express.static(__dirname + root));
+
+app.get("/test",function(request,response){
+    var param=request.query.username
+    console.log('Get requested by '+param)
+    response.send('Thank you for requesting our Get Service')
+})
+
+app.post('/test',function(request,response){
+    console.log(request.body)
+    var data=request.body;
+    console.log('Post requested, here is the data :'+data)
+    response.send('Thank you for requesting our Post Service')
 });
 
+app.post('/calculator', (req, res) => {
+    let data = req.body;
+    console.log(req)
+    let n1 = parseInt(data.number1);
+    let n2 = parseInt(req.body.number2);
+    let operator = req.body.operator;
 
-app.listen(3000);
+    let result;
+    switch(operator){
+        case "+":
+            result = n1 + n2;
+            break;
+        
+        case "-":
+            result = n1 - n2;
+            break;
+        
+        case "*":
+            result = n1 * n2;
+            break;
+        
+        case "/":
+            result = n1 / n2;
+            break;
+        
+        default: 
+            result = null;
+            break;
+    }   
 
-const getTimeAndServerActivityStamp = () => {
-    let date = new Date();
-    let stamp = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} [Server Activity] : `;
-    return stamp;
-}
+    if (result == null){
+        res.status(400).send({error: "Incorrect calculator operator."});
+    } else {
+        res.status(200).send({answer: result});
+    }
 
-console.log(`${getTimeAndServerActivityStamp()} Server Started`);
+    
+});
+
+log.info("some info message %s", "injected string");
+
+
+//start the app and listen to the port
+app.listen(port);
+console.log("Listening on port ", port);
